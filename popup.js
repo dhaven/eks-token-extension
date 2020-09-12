015bc902a3392ce8cb4cac996b53f514da16237f
David Haven <david.haven@gonitro.com>
2020-09-12 16:07:16 +0100

    add support for different clusters

--- popup.js
+++ popup.js
@@ -1,16 +1,28 @@
-let getToken = document.getElementById('button');
-
-getToken.addEventListener('click', function (event) {
-  let accessKey = document.getElementById('accesskey').value
-  let secretKey = document.getElementById('secretkey').value
-  const token = get_bearer_token(accessKey, secretKey)
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {file: 'contentScript.js'},
-        function() {
-          chrome.tabs.sendMessage(tabs[0].id, token);
-          window.close();
-        });
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  window.resizeTo(300,200)
+  document.getElementById('button').addEventListener('click', function (event) {
+    let accessKey = document.getElementById('accesskey').value
+    let secretKey = document.getElementById('secretkey').value
+    let clustername = document.getElementById('clustername').value
+    let autoFillPassword = document.getElementById("checkbox").checked;
+    const token = get_bearer_token(accessKey, secretKey,clustername)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      if(accessKey === '' || secretKey === '' || clustername === ''){
+        location.reload();
+      }else{
+        if(autoFillPassword){
+          chrome.tabs.executeScript(
+            tabs[0].id,
+            {file: 'contentScript.js'},
+            function() {
+              chrome.tabs.sendMessage(tabs[0].id, token);
+              window.close();
+            }
+          );
+        }else {
+          document.getElementById('content_1').innerHTML += '<p>'+token+'</p>';
+        }
+      }
+    });
+  })
 })
